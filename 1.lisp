7dcfdedbf1ba4edf27c90bf4edb3a9c8ed32cf89
Martin Kielhorn <kielhorn.martin@gmail.com>
2017-09-30 22:37:13 +0000

    add more headers so that the types can be defined
    i use this command to check for error messages:
    
    c2ffi /tmp/frame1.h -i /usr/include/arm-linux-gnueabihf -D null -M
    /tmp/tmpCEL1YIM3.tmp -A arm-pc-linux-gnu 2>&1|less

--- 1.lisp
+++ 1.lisp
@@ -24,16 +24,25 @@
 		     :if-does-not-exist :create
 		     :if-exists :supersede)
 
-    ;;(format s "#include <sys/types.h>~%")
+    ;;(format s "#include <asm-generic/int-ll64.h>~%")
+    ;;(format s "#include <linux/types.h>~%")
+    (format s "#include <sys/types.h>~%")
+
+    
+    (format s "#include <linux/i2c.h>~%")
+    
     ;;(format s "#include <sys/stat.h>~%")
-    ;;(format s "#include <sys/ioctl.h>~%")
+    (format s "#include <sys/ioctl.h>~%")
+    (format s "#include <linux/fb.h>~%")
+    (format s "#include <stdint.h>~%") ;; uint32_t
     (format s "#include \"/tmp/frame0.h\"~%")
     (format s "#include \"/tmp/frame_macros.h\"~%")))
-
+  
 (c-include "/tmp/frame1.h"
 	   :trace-c2ffi t
 	   ;; :spec-path "/tmp"
 	   ;;:exclude-sources ("")
+	   :sysincludes '("/usr/include/arm-linux-gnueabihf/")
 	   :include-sources ("/usr/include/linux/ioctl.h"
 			     "/usr/include/linux/fb.h")
 	   :exclude-arch ("i386-unknown-freebsd"
@@ -49,77 +58,86 @@
 						      
 			  ))
 
+(with-open-file (s "/dev/graphics/fb0" :direction :input
+		   :element-type '(unsigned-byte 8))
+  (let ((fd (sb-sys:fd-stream-fd s)))
+    (sb-posix:ioctl fd +FBIOGET-FSCREENINFO+ )
+    #+nil (sb-posix:mmap )))
 
 
-(defctype off-t :int "C type off_t")
 
 
 
-;; 'prot' protection flags for mmap.
-(defbitfield mmap-prot-flags
+
+  #+nil
+(progn
+  (defctype off-t :int "C type off_t")
+  ;; 'prot' protection flags for mmap.
+  (defbitfield mmap-prot-flags
     (:prot-none #x00)
-  (:prot-read #x04)
-  (:prot-write #x02)
-  (:prot-exec #x01))
+    (:prot-read #x04)
+    (:prot-write #x02)
+    (:prot-exec #x01))
 
-;; Specifies the type of the object in mmap 'flags'.
-(defbitfield mmap-map-flags
+  ;; Specifies the type of the object in mmap 'flags'.
+  (defbitfield mmap-map-flags
     (:map-file #x0001)
-  (:map-type #x000f)
-  (:map-shared #x0010)
-  (:map-private #x0000)
-  (:map-fixed #x0100)
-  (:map-noextend #x0200)
-  (:map-hassemphore #x0400)
-  (:map-inherit #x0800)
-  (:map-anon #x0002))
+    (:map-type #x000f)
+    (:map-shared #x0010)
+    (:map-private #x0000)
+    (:map-fixed #x0100)
+    (:map-noextend #x0200)
+    (:map-hassemphore #x0400)
+    (:map-inherit #x0800)
+    (:map-anon #x0002))
 
-(defcfun ("mmap" %mmap)
-    :pointer
-  (addr :pointer)
-  (len :unsigned-int)
-  (prot mmap-prot-flags)
-  (flags mmap-map-flags)
-  (filedes :int)
-  (off off-t))
+  (defcfun ("mmap" %mmap)
+      :pointer
+    (addr :pointer)
+    (len :unsigned-int)
+    (prot mmap-prot-flags)
+    (flags mmap-map-flags)
+    (filedes :int)
+    (off off-t))
 
-(defun mmap (addr len prot flags filedes off)
-  "Map files or devices into memory."
-  (let ((ptr (%mmap addr len prot flags filedes off)))
+  (defun mmap (addr len prot flags filedes off)
+    "Map files or devices into memory."
+    (let ((ptr (%mmap addr len prot flags filedes off)))
 					; Mmap returns -1 in case of error
-    (if (= (pointer-address ptr) #xffffffff)
-	nil
-	ptr)))
+      (if (= (pointer-address ptr) #xffffffff)
+	  nil
+	  ptr)))
 
-(defcfun ("munmap" %munmap) :int
-  (addr :pointer)
-  (len :unsigned-int))
+  (defcfun ("munmap" %munmap) :int
+    (addr :pointer)
+    (len :unsigned-int))
 
-(defun munmap (addr len)
-  "Remove a mapping."
-  (cond
-    ((zerop len) t)
-    ((null addr) nil)
-    (t
-     (let ((result (%munmap addr len)))
+  (defun munmap (addr len)
+    "Remove a mapping."
+    (cond
+      ((zerop len) t)
+      ((null addr) nil)
+      (t
+       (let ((result (%munmap addr len)))
 					; In case of success, munmap returns 0.
-       (= result 0)))))
+	 (= result 0)))))
 
-(defbitfield open-flags
+  (defbitfield open-flags
     (:rdonly #x0000)
-  :wronly              ;#x0001
-  :rdwr                ;&hellip;
-  :nonbloc
-  :append
-  (:creat  #x0200))
+    :wronly              ;#x0001
+    :rdwr                ;&hellip;
+    :nonbloc
+    :append
+    (:creat  #x0200))
 
-(defcfun ("open" %open) :int
-  (path :string)
-  (flags open-flags)
-  (mode :uint16)) ; unportable
+  (defcfun ("open" %open) :int
+    (path :string)
+    (flags open-flags)
+    (mode :uint16)) ; unportable
 
 
-#+nil
+  )
+  #+nil
 (defparameter *fd* (%open "/dev/graphics/fb0" '(:rdwr) #o644))
 
 
